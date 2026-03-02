c9c71e3047036ea680e90e5311ecf434d5eeba4b
Daniel <212781679+ChorusAV1@users.noreply.github.com>
2026-03-02 07:15:59 -0500

    Added login page

--- timeboxing-frontend/src/App.tsx
+++ timeboxing-frontend/src/App.tsx
@@ -6,7 +6,7 @@ import Scheduler from "./pages/Scheduler"
 function App()
 {
 	return (
-		<>
+		<div className="flex flex-col h-screen">
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<Login/>}/>
@@ -15,7 +15,7 @@ function App()
 					<Route path="*" element={<Navigate to="/"/>}/>
 				</Routes>
 			</BrowserRouter>
-		</>
+		</div>
 	)
 }
 
--- timeboxing-frontend/src/pages/Login.tsx
+++ timeboxing-frontend/src/pages/Login.tsx
@@ -1,9 +1,39 @@
-import React, { JSX } from 'react'
+import React, { type JSX } from 'react'
 
 const Login: React.FC = (): JSX.Element =>
 {
     return (
-        <div>Login</div>
+        <div className='bg-[#202020] flex flex-col items-center justify-center grow'>
+            <div className='bg-[#303030] h-100 w-80 rounded-2xl text-white flex flex-col items-center justify-center'>
+
+                <h1 className='text-2xl'>Timeboxing</h1>
+
+                <div className='h-15'/>
+
+                <h2 className='text-xl'>Inicia sesión</h2>
+
+                <div className='h-10'/>
+
+                <div>
+                    <label className='flex flex-col'>Email</label>
+                    <input
+                        name='Email'
+                        className='bg-[#181818] rounded p-2'
+                    />
+                </div>
+
+                <div className='h-5'/>
+                    
+                <div>
+                    <label className='flex flex-col'>Contraseña</label>
+                    <input
+                        name='Contrañesa'
+                        className='bg-[#181818] rounded p-2'
+                    />
+                </div>
+                
+            </div>
+        </div>
     )
 }
 
